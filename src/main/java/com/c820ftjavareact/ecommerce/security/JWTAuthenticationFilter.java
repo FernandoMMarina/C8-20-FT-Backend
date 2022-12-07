d7e115f285ec3987f7efadb882781d45221d4adb
Fernando Marina <fernando.m.marina@gmail.com>
2022-12-07 17:47:44 -0300

    jwt

--- src/main/java/com/c820ftjavareact/ecommerce/security/JWTAuthenticationFilter.java
+++ src/main/java/com/c820ftjavareact/ecommerce/security/JWTAuthenticationFilter.java
@@ -41,7 +41,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
       String token = TokenUtils.createToken(userDetails.getNombre(),userDetails.getUsername());
 
       response.addHeader("Authorization", "Bearer" + token);
-      response.addHeader("Text/plain", "Token" + token );
+     //response.addHeader("Text/plain", "Token" + token );
       response.getWriter().flush();
       super.successfulAuthentication(request,response,chain,authResult);
     }
